0ea9f937f4006a6a9b0e173cb6be142e6f9b26b7
Furqan Asghar <furqan.790@outlook.com>
2023-02-19 10:08:23 +0500

    fix: issue token inventory auto fetch field for drafts

--- token_enhancement/token_enhancement/doctype/issue_token_inventory/issue_token_inventory.js
+++ token_enhancement/token_enhancement/doctype/issue_token_inventory/issue_token_inventory.js
@@ -3,8 +3,10 @@
 
 frappe.ui.form.on('Issue Token Inventory', {
 	onload: function(frm) {
-        frm.set_value('issued_by', frappe.session.user);
-        frm.set_value('issue_date', frappe.datetime.now_date());
+		if (frm.is_new() || frm.doc.docstatus == 0) {
+			frm.set_value('issued_by', frappe.session.user);
+        	frm.set_value('issue_date', frappe.datetime.now_date());
+		}
 		frm.set_query("token_tracer", "issue_tokens", function() {
 			return {
 				filters: {
