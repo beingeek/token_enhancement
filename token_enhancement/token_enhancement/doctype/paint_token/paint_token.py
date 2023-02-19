581a057024be54892b3236b8d03d02d305076e46
Furqan Asghar <furqan.790@outlook.com>
2023-02-19 10:16:57 +0500

    feat: enhanced update_token function for token redeem and token issue

--- token_enhancement/token_enhancement/doctype/paint_token/paint_token.py
+++ token_enhancement/token_enhancement/doctype/paint_token/paint_token.py
@@ -40,26 +40,44 @@ class PaintToken(Document):
 	def generate_token_key(self):
 		return frappe.generate_hash(length=10)
 
-	def update_token(self, issue_token, is_issued=0, update=False, update_modified=False):
+	def update_token(self, issue_token=None, is_issued=0, payment_entry=None, is_redeemed=0, update=False, update_modified=False):
 		if issue_token:
 			if is_issued:
-				self.is_issued = is_issued
 				self.issued_to = issue_token.customer
 				self.issue_date = issue_token.issue_date
 				self.issued_by = issue_token.issued_by
+				self.issue_token_document = issue_token.name
 
 			elif not is_issued:
-				self.is_issued = is_issued
 				self.issued_to = None
 				self.issue_date = None
 				self.issued_by = None
+				self.issue_token_document = None
+
+		if payment_entry:
+			if is_redeemed:
+				self.redeemed_by = payment_entry.owner
+				self.redeemed_date = payment_entry.creation
+				self.redeem_payment_entry = payment_entry.name
+
+			elif not is_redeemed:
+				self.redeemed_by = None
+				self.redeemed_date = None
+				self.redeem_payment_entry = None
 
 		if update:
-			self.db_set('is_issued', self.is_issued, update_modified=update_modified)
-			self.db_set('issued_to', self.issued_to, update_modified=update_modified)
-			self.db_set('issue_date', self.issue_date, update_modified=update_modified)
-			self.db_set('issued_by', self.issued_by, update_modified=update_modified)
+			if issue_token:
+				self.db_set('is_issued', is_issued, update_modified=update_modified)
+				self.db_set('issued_to', self.issued_to, update_modified=update_modified)
+				self.db_set('issue_date', self.issue_date, update_modified=update_modified)
+				self.db_set('issued_by', self.issued_by, update_modified=update_modified)
+				self.db_set('issue_token_document', self.issue_token_document, update_modified=update_modified)
 
+		if payment_entry:
+			self.db_set('is_redeemed', is_redeemed, update_modified=update_modified)
+			self.db_set('redeemed_by', self.redeemed_by, update_modified=update_modified)
+			self.db_set('redeemed_date', self.redeemed_date, update_modified=update_modified)
+			self.db_set('redeem_payment_entry', self.redeem_payment_entry, update_modified=update_modified)
 
 def qr_code_generator(doc, qr_field):
 	qr_field_value = doc.get(qr_field)
